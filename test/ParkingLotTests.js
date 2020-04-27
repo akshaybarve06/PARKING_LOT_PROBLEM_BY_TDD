2a4d0bcfeebb825f024d78be9c411d66fd35978c
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-27 20:21:44 +0530

    Modified Test Cases, Changed Some Declerations

--- test/ParkingLotTests.js
+++ test/ParkingLotTests.js
@@ -1,30 +1,29 @@
 // Importing 'chai' library and 'Main Class' in code
 let assert = require('chai').assert;
 let parkingLotMain = require('../modules/ParkingLotMainClass')
+let car = new Object();
 
 //Test Cases For Parking Lot System
 describe(`Test Cases For Parking Lot System`, () =>
 {
     // Test For Add Vehicle in Parking Lot
-    it.skip(`given car object when car is parked should return true`, () =>
+    it(`given car object when car is parked should return true`, () =>
     {
-        let car = {};
         let carParkedOrNot= parkingLotMain.isParked(car);
         assert.isTrue(carParkedOrNot);
     })
    // Test For Possiblities To Add Vehicle in Parking Lot
-   it.skip(`given car object when invalid and car is not parked should return exception`, () =>
+   it(`given car object when invalid and car is not parked should return exception`, () =>
    {
        try{
             let carParkedOrNot= parkingLotMain.isParked(null);
-            assert.equal(true,carParkedOrNot);
+            assert.isTrue(carParkedOrNot);
        }catch(e){
            console.log(e.message);
        }
    })
    // Test For Unpark Car from parking Lot
-   it.skip(`given car object when car is unpark then return true`, () =>{
-        let car = {};
+   it(`given car object when car is unpark then return true`, () =>{
         parkingLotMain.isParked(car);
         let carParkedOrNot= parkingLotMain.isUnparked(car);
         assert.isTrue(carParkedOrNot);
@@ -33,11 +32,9 @@ describe(`Test Cases For Parking Lot System`, () =>
    it(`given car object when invalid or car can't unparked should return exception`, () =>
    {
        try{
-        let car = {};
-        let car2 ={};
-        parkingLotMain.isParked(car);
-        let carParkedOrNot= parkingLotMain.isUnparked(undefined);
-        assert.isTrue(carParkedOrNot);
+            parkingLotMain.isParked(car);
+            let carParkedOrNot= parkingLotMain.isUnparked(undefined);
+            assert.isTrue(carParkedOrNot);
        }catch(e){
            console.log(e.message);
        }
