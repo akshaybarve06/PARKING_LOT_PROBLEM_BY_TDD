7716dffe4d840530e9a57d8904763b39894625ee
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-28 16:29:58 +0530

    Added Method To Check Parking Lot is Full

--- modules/ParkingLotOwner.js
+++ modules/ParkingLotOwner.js
@@ -1,4 +1,14 @@
+let parkingCapacity=2;
+
 class ParkingLotOwner{
 
+    checkParkingFull(parking,callback){
+        if (parking.length == parkingCapacity){
+            console.log('Hey Sanjay..Parking Is Full Could not park more Vehicle')
+            callback(false)
+        }
+        else
+            callback(true)
+    }
 }
-module.exports=new ParkingLotOwner;
+module.exports =new ParkingLotOwner
