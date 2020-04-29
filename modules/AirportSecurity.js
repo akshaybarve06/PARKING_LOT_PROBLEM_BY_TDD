3652c6a6e6a921040ea1bcdb018fd49ad021311c
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-29 13:24:28 +0530

    Added Method Send Notification To Notify Airport Security

--- modules/AirportSecurity.js
+++ modules/AirportSecurity.js
@@ -1,5 +1,11 @@
 // Airport Security Class To Send Notification To Airport If Parking is Full
 class AirportSecurity{
 
+    // Method To Send Notifation TO Airport When Parking Is Full
+    sendNotification=(function(result){
+        if (result==false)
+            console.log("Notification From Parking Lot : Parking Is Full..!")
+    })
+
 }
 module.exports=new AirportSecurity;
